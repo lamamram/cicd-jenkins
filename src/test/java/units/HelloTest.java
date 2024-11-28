a2986b1023049f2f0dabd661ab1c4ab5934d8837
mlamamra <mlamamra@dawan.fr>
2024-11-28 13:50:50 +0100

    integration tests OK

---
+++ src/test/java/units/HelloTest.java
@@ -0,0 +1,67 @@
+package units;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import com.example.helloworld.Hello;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+/**
+ * Unit test for Hello.
+ * <p/>
+ * A unit test aims to test all code and code paths of a specific class.
+ */
+public class HelloTest {
+
+  @Test
+  @Tag("Unit")
+  public void testSayHello() {
+    OutputStream os = new ByteArrayOutputStream();
+    PrintStream stream = new PrintStream(os, true);
+
+    Hello hi = new Hello();
+    hi.sayHello(stream);
+
+    assertThat(os.toString(),
+        is(equalTo(String.format("%s%s", Hello.HELLO, System.lineSeparator()))));
+  }
+
+  @Test
+  @Tag("Unit")
+  public void testSayHelloAFewTimes() {
+    OutputStream os = new ByteArrayOutputStream();
+    PrintStream stream = new PrintStream(os, true);
+
+    Hello hi = new Hello();
+    hi.setTimes(3);
+    hi.sayHello(stream);
+
+    // Does it say "Hello!" three times?
+    String goal = String.format("%1$s%2$s%1$s%2$s%1$s%2$s", Hello.HELLO, System.lineSeparator());
+    assertThat(os.toString(), is(equalTo(goal)));
+  }
+
+  @Test
+  @Tag("Unit")
+  public void testIllegalArgumentForHelloTooMuch() {
+    Hello hi = new Hello();
+    assertThrows(IllegalArgumentException.class,
+        () -> hi.setTimes(Hello.MAXIMUM_AMOUNT_OF_TIMES + 1));
+  }
+
+  @Test
+  @Tag("Unit")
+  public void testIllegalArgumentForHelloNegative() {
+    Hello hi = new Hello();
+    assertThrows(IllegalArgumentException.class, () -> hi.setTimes(-1));
+  }
+}
---
+++ src/test/java/units/HelloWithTestsIT.java
@@ -0,0 +1,60 @@
+package units;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import com.example.helloworld.Hello;
+import com.example.helloworld.HelloApp;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Integration test for the HelloApp program.
+ * <p>
+ * An integration test verifies the workings of a complete program, a module, or a set of dependant
+ * classes.
+ */
+public class HelloWithTestsIT {
+  private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+
+  @BeforeEach
+  public void before() {
+    // By putting our own PrintStream in the place of the normal System.out,
+    // the output produced by the application can be verified.
+    System.setOut(new PrintStream(out));
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    // Restore the original System.out to prevent weirdness in any following tests.
+    System.setOut(originalOut);
+  }
+
+  @Test
+  @Tag("IT")
+  public void doesItSayHelloTest() {
+    String[] args = {"1"};
+    HelloApp.main(args);
+
+    assertThat(out.toString(), is(String.format("%s%s", Hello.HELLO, System.lineSeparator())));
+  }
+
+  @Test
+  @Tag("IT")
+  public void doesItSayHelloTest3() {
+    String[] args = {"3"};
+    HelloApp.main(args);
+
+    // Hello
+    // Hello
+    // Hello
+    String thrice = String.format("%1$s%2$s%1$s%2$s%1$s%2$s", Hello.HELLO, System.lineSeparator());
+    assertThat(out.toString(), is(thrice));
+  }
+}
